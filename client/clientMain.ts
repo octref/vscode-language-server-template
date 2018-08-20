04154c1717b1ca7317a6dceb89b5fac23ce063f6
Pine Wu <octref@gmail.com>
2018-08-20 00:28:45 -0700

    Simple streaming

--- client/clientMain.ts
+++ client/clientMain.ts
@@ -1,5 +1,6 @@
 import * as path from 'path'
 import * as vscode from 'vscode'
+import * as WebSocket from 'ws'
 
 import {
   LanguageClient,
@@ -11,6 +12,14 @@ import {
 export function activate(context: vscode.ExtensionContext) {
   const serverModule = context.asAbsolutePath(path.join('server', 'dist', 'serverMain.js'))
   const debugOptions = { execArgv: ['--nolazy', '--inspect=6006'] }
+
+  const config = vscode.workspace.getConfiguration('mls.debug.log')
+  let socket: WebSocket = undefined;
+  if (config) {
+    if (config.output === 'websocket' && config.port) {
+      socket = new WebSocket(`ws://localhost:${config.get('port')}`)
+    }
+  }
 
   const serverOptions: ServerOptions = {
     run: { module: serverModule, transport: TransportKind.ipc },
@@ -28,11 +37,28 @@ export function activate(context: vscode.ExtensionContext) {
     ],
     synchronize: {
       configurationSection: []
-    }
+    },
+    outputChannel: socket ? socketToChannel(socket) : undefined
   }
 
   const client = new LanguageClient('mls', 'Markdown Language Server', serverOptions, clientOptions)
 
   const disposable = client.start()
   context.subscriptions.push(disposable)
+}
+
+const socketToChannel = (socket: WebSocket): vscode.OutputChannel => {
+  return {
+    name: 'websocket',
+    append(value: string) {
+      socket.send(value)
+    },
+    appendLine(value: string) {
+      socket.send(value)
+    },
+    clear() {},
+    show() {},
+    hide() {},
+    dispose() {}
+  }
 }
